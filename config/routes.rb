d24bfb531670abf5175eb638f706114b5fc076c6
Jackie <jye0325@gmail.com>
2022-10-24 11:25:00 -0400

    Create Inventory model and seeds for backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :inventories
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
