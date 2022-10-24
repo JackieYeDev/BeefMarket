0008056aa038c0e85da31d81a17452b38ebaf0e8
Jackie <jye0325@gmail.com>
2022-10-24 10:46:37 -0400

    Update content display to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Container, Segment } from "semantic-ui-react";
+import { Container, Grid, Segment } from "semantic-ui-react";
 import Cart from "./Cart";
 import Inventory from "./Inventory";
 import { UserContext } from "../context/user";
@@ -8,24 +8,29 @@ function Home(props) {
   const [user] = useContext(UserContext);
 
   return (
-    <>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="top">
       {!user && (
-        <Segment raised>
-          <Container text>
+        <Grid.Column style={{ maxWidth: 500 }}>
+          <Segment raised>
             <p>Welcome to Beef Market!</p>{" "}
             <p>
               To get started, please sign up or login to an existing account!
             </p>
-          </Container>
-        </Segment>
+          </Segment>
+        </Grid.Column>
       )}
       {user && (
-        <>
-          <Cart />
-          <Inventory />
-        </>
+        <Grid.Column style={{ maxWidth: 700 }}>
+          <Segment raised>
+            <p>Welcome to Beef Market!</p>{" "}
+            <p>
+              You can view our beef selections in our inventory, view an
+              existing order, or checkout your cart.
+            </p>
+          </Segment>
+        </Grid.Column>
       )}
-    </>
+    </Grid>
   );
 }
 
