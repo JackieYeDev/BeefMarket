6e7fbec7cd26e30e69d9430eb27ca192974ad6fc
Jackie <jye0325@gmail.com>
2022-10-24 10:43:02 -0400

    Add error message display to SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Button, Checkbox, Form, Label, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Grid,
+  Header,
+  Label,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
 
 function SignupForm(props) {
@@ -8,6 +17,8 @@ function SignupForm(props) {
     password: "",
     passwordConfirmation: "",
   });
+
+  const [errors, setErrors] = useState([]);
 
   let history = useHistory();
 
@@ -25,51 +36,73 @@ function SignupForm(props) {
       }),
     })
       .then((r) => r.json())
-      .then(() => history.push("/"))
-      .catch((err) => console.error(err));
+      .then((response) => {
+        if (response.status === 200) {
+          setErrors([]);
+          history.push("/");
+        } else setErrors(response.errors);
+      });
   }
 
   return (
-    <Segment>
-      <Form onSubmit={handleSubmit}>
-        <Form.Field>
-          <label>Username (Your email address will be your username):</label>
-          <input
-            placeholder={"Please enter an email address!"}
-            value={formData.username}
-            onChange={(e) => {
-              setFormData({ ...formData, username: e.target.value });
-            }}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Password: </label>
-          <input
-            type={"password"}
-            placeholder={"Please enter a password with at least 6 characters!"}
-            value={formData.password}
-            onChange={(e) => {
-              setFormData({ ...formData, password: e.target.value });
-            }}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Confirm Password: </label>
-          <input
-            type={"password"}
-            placeholder={"Please enter the same password again!"}
-            value={formData.passwordConfirmation}
-            onChange={(e) => {
-              setFormData({
-                ...formData,
-                passwordConfirmation: e.target.value,
-              });
-            }}
-          />
-        </Form.Field>
-        <Button type={"submit"}>Register</Button>
-      </Form>
-    </Segment>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="red" textAlign="center">
+          Signup for your free account
+        </Header>
+        <Segment>
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <label>
+                Username (Your email address will be your username):
+              </label>
+              <input
+                placeholder={"Please enter an email address!"}
+                value={formData.username}
+                onChange={(e) => {
+                  setFormData({ ...formData, username: e.target.value });
+                }}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Password: </label>
+              <input
+                type={"password"}
+                placeholder={
+                  "Please enter a password with at least 6 characters!"
+                }
+                value={formData.password}
+                onChange={(e) => {
+                  setFormData({ ...formData, password: e.target.value });
+                }}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Confirm Password: </label>
+              <input
+                type={"password"}
+                placeholder={"Please enter the same password again!"}
+                value={formData.passwordConfirmation}
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    passwordConfirmation: e.target.value,
+                  });
+                }}
+              />
+            </Form.Field>
+            <Button type={"submit"}>Register</Button>
+          </Form>
+          {errors.length > 0 && (
+            <Message>
+              {errors.map((err, index) => (
+                <p key={index}>{err}</p>
+              ))}
+            </Message>
+          )}
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 }
 
